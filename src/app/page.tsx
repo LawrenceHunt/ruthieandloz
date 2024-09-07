b50162e8abacae286ac60a687f540ad994b6e8f2
Lawrence Hunt <huntlawrence@gmail.com>
2024-09-07 23:49:35 +0700

    page styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import { RSVPForm } from "~/components/RSVPForm";
 export default async function Home() {
   return (
     <HydrateClient>
-      <main className="my-8">
-        <p>Dearest friends</p>
+      <main className="pl-8">
+        <p className="mt-8">Dearest friends</p>
 
         <p className="">Please join</p>
 
@@ -19,13 +19,13 @@ export default async function Home() {
 
         <p className="">on the occasion of their wedding</p>
 
-        <div>
+        <div className="mb-8">
           <p>26th July 2025</p>
           <p>Ewen, Gloucestershire</p>
         </div>
-      </main>
 
-      <RSVPForm />
+        <RSVPForm />
+      </main>
     </HydrateClient>
   );
 }
