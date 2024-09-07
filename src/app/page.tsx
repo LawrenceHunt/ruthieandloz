33e4d0712aef1690f34f19cb25466dd38580da0b
Lawrence Hunt <huntlawrence@gmail.com>
2024-09-07 23:47:41 +0700

    primitive styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,20 @@ import { RSVPForm } from "~/components/RSVPForm";
 export default async function Home() {
   return (
     <HydrateClient>
-      <main>
+      <main className="my-8">
         <p>Dearest friends</p>
 
-        <p className="text-3xl">Please join</p>
+        <p className="">Please join</p>
 
         <div className="w-full">
-          <h1 className="flex flex-col">
+          <h1 className="flex gap-1">
             <span className="">Ruth</span>
             <span className="">&</span>
             <span className="">Lawrence</span>
           </h1>
         </div>
 
-        <p className="text-3xl">on the occasion of their wedding</p>
+        <p className="">on the occasion of their wedding</p>
 
         <div>
           <p>26th July 2025</p>
